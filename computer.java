82679d70c4b0d6fa8c0b8f0d8f40139a11214eea
kabilak468 <kabik468@gmail.com>
2026-08-16 19:33:12 +0530

    abstraction

---
+++ computer.java
@@ -0,0 +1,15 @@
+//abstract class
+abstract class computer {
+    void print(){
+        System.out.println("Printing 'Hello'");
+    }
+}
+//Abstract class cannot be accessed directly..instead we can inherit the class and use it...
+class hp extends computer{
+}
+class Abstract{
+    public static void main(String[] args) {
+        hp obj=new hp();
+        obj.print();
+    }
+}
