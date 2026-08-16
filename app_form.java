92e125d54f44b837b902585c3a6cc84c8973104c
kabilak468 <kabik468@gmail.com>
2026-08-16 16:27:27 +0530

    Passing values through methods

--- app_form.java
+++ app_form.java
@@ -1,15 +1,26 @@
 public class app_form {
     int rollNo;
     String name;
-    public static void main(String[] args) {
-        //Creating an object
-        app_form obj=new app_form();
-        obj.name="Kabi";
-        obj.rollNo=167;
-        obj.display();
-    }
     public void display(){
         System.out.println(name);
         System.out.println(rollNo);
+    }
+    public void setValues(String str,int data){
+        name=str;
+        rollNo=data;
+    }
+}
+class Main{
+     public static void main(String[] args) {
+        //Creating an object
+        app_form obj1=new app_form();
+        //Passing values through fields
+        obj1.name="Kabi";
+        obj1.rollNo=167;
+        obj1.display();
+        //Passing values through methods
+        app_form obj2 = new app_form();
+        obj2.setValues("Dhanu",130);
+        obj2.display();
     }
 }
