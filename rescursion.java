7da06b1b47c76d80eb84953c328d15776816bfad
kabilak468 <kabik468@gmail.com>
2026-08-16 15:59:46 +0530

    Recursion

---
+++ rescursion.java
@@ -0,0 +1,15 @@
+public class rescursion {
+    public static void main(String[] args) {
+        natural(10);
+    }
+    public static void natural(int n){
+        //Base case
+        if(n==1){
+            System.out.println(1);
+        }//Recursive case
+        else{
+            System.out.println(n);
+            natural(n-1);
+        }
+    }
+}
