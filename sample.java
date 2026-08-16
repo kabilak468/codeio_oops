2f0dab7573f2fed5d42287946317e0f44f8d6aa4
kabilak468 <kabik468@gmail.com>
2026-08-16 17:55:57 +0530

    Parameterised and Non-parameterised constructor

---
+++ sample.java
@@ -0,0 +1,27 @@
+public class sample {
+    String name;
+    int rollNo;
+
+    //Parameterised Constructor
+    sample(String user,int roll){
+        name=user;
+        rollNo=roll;
+    }
+    
+    //Non-Parameterised Constructor creation
+
+    /*
+    sample(){
+        //Always used as default values
+        name="Kabi";
+        rollNo=167;
+    }
+    */
+}
+class constructor{
+    public static void main(String[] args) {
+        sample sp=new sample("Kabi",167);
+        System.out.println(sp.name);
+        System.out.println(sp.rollNo);
+    }
+}
