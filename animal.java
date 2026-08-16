8d0a3eb1a207b3ba83a2b78aec168149a482e911
kabilak468 <kabik468@gmail.com>
2026-08-16 18:16:00 +0530

    Inheritance carried out

--- animal.java
+++ animal.java
@@ -1,3 +1,32 @@
+//Filename=Parentname
+
+//Parent class
 public class animal {
-    
+    public void leg(){
+        System.out.println("It has four legs");
+    }
+    public void walk(){
+        System.out.println("It can walk");
+    }
+}
+//Child1 class
+class dog extends animal{
+    public void sound(){
+        System.out.println("Says bow");
+    } 
+}
+//Child2 class
+class cat extends animal{
+    public void sound(){
+        System.out.println("Says meow");
+    }
+}
+
+class inherit{
+    public static void main(String[] args) {
+        dog obj =new dog();
+        obj.leg();
+        obj.walk();
+        obj.sound();
+    }
 }
