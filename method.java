0d1db854578a58eb3ac42421d21506b1abfb45c0
kabilak468 <kabik468@gmail.com>
2026-08-16 15:45:42 +0530

    Updated methods

--- method.java
+++ method.java
@@ -3,9 +3,11 @@ public class method{
         display();
         System.out.println(add(5,10));
     }
+    //void function with no parameters
     public static void display(){
         System.out.println("Displaying sum !");
     }
+    //non-void function with parameters
     public static int add(int a,int b){
         int c=a+b;
         return c;
