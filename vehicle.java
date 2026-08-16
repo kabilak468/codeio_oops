f0bc69a5c4de155ec066d2ba76f7b5a077ccfc8a
kabilak468 <kabik468@gmail.com>
2026-08-16 21:26:12 +0530

    Static keyword at variables

--- vehicle.java
+++ vehicle.java
@@ -1,13 +1,17 @@
-public class vehicle {
+public class vehicle{
+    static int mil=56;
     static void wheels(){
         System.out.println("4 wheels");
     }
 }
 class bus extends vehicle{
-
+    
 }
 class Newthing{
     public static void main(String[] args) {
         vehicle.wheels();
+        System.out.println(vehicle.mil);
+        bus.mil=45;
+        System.out.println(vehicle.mil);
     }
 }
